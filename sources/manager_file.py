b056eb9c2ee66f56535640bfc3a4ff45fba23aed
rodrigo-apoc <rodrigof.lops@gmail.com>
2026-07-16 00:18:13 -0300

    test

--- sources/manager_file.py
+++ sources/manager_file.py
@@ -3,7 +3,6 @@ from os.path import join, isfile, dirname
 from pickle import load as load_pickle, dump as dump_pickle
 from json import load as load_json
 from typing import Dict, Optional, Any
-from humanize.i18n import activate
 
 from manager_environment import EnvironmentManager as EM
 
@@ -14,7 +13,6 @@ def init_localization_manager():
     Load GUI translations JSON file.
     """
     FileManager.load_localization("translation.json")
-    #FileManager.activate_locale()
 
 
 class FileManager:
@@ -26,10 +24,6 @@ class FileManager:
     ASSETS_DIR = "assets"
     _LOCALIZATION: Dict[str, str] = dict()
 
-    HUMANIZE_LOCALE_MAP = {
-        "pt": "pt_BR",
-    }
-
     @staticmethod
     def load_localization(file: str):
         """
@@ -40,20 +34,6 @@ class FileManager:
         with open(join(dirname(__file__), file), encoding="utf-8") as config_file:
             data = load_json(config_file)
         FileManager._LOCALIZATION = data[EM.LOCALE]
-    
-    @staticmethod
-    def activate_locale():
-        """
-        Activate localization for humanize library.
-
-        Uses current locale defined by environmental variable.
-        """
-        activate(
-            FileManager.HUMANIZE_LOCALE_MAP.get(
-                EM.LOCALE,
-                EM.LOCALE,
-            )
-        )
 
     @staticmethod
     def t(key: str) -> str:
